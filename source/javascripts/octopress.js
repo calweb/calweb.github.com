836a037e4a8af39e9f6fac6829aa007d31299106
Calvin Webster <calweb@gmail.com>
2012-08-09 16:13:31 -0400

    revert

--- source/javascripts/octopress.js
+++ source/javascripts/octopress.js
@@ -123,25 +123,6 @@ $.domReady(function() {
   addCodeLineNumbers();
   getNav();
   addSidebarToggler();
-
-    /*
-     Cornify: http://www.cornify.com
-     http://paulirish.com/2009/cornify-easter-egg-with-jquery/
-
-     */
-
-    var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
-    $(document).keydown(function(e) {
-        kkeys.push( e.keyCode );
-        if ( kkeys.toString().indexOf( konami ) >= 0 ){
-            $(document).unbind('keydown',arguments.callee);
-            $.getScript('http://www.cornify.com/js/cornify.js',function(){
-                cornify_add();
-                $(document).keydown(cornify_add);
-            });
-        }
-    });
-
 });
 
 // iOS scaling bug fix
